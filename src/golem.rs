b4c507787ac579bb56b8b22bdbf65fedf05802fd
Rafal Piotrowski <rafalpiotrowski@users.noreply.github.com>
2021-08-30 14:58:23 +0200

    showing balances in seperate thread

---
+++ src/golem.rs
@@ -0,0 +1,61 @@
+use std::str::FromStr;
+use std::{collections::HashMap, env};
+use secp256k1::SecretKey;
+
+pub struct GolemToken {
+    pub network: web3::Web3<web3::transports::Http>,
+    pub accounts: HashMap<&'static str, Account>
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Account {
+    pub address: web3::types::Address,
+    pub pk: SecretKey
+}
+
+impl GolemToken {
+    pub fn new(network_url: &str) -> Self {
+        GolemToken {
+            network : {
+                let transport = web3::transports::Http::new(network_url).unwrap();
+                web3::Web3::new(transport)
+            },
+            accounts: HashMap::new()
+        }
+    }
+
+    pub async fn initialize_accounts(&mut self) -> web3::error::Result {
+        let account1_address = env::var("ACCOUNT1_ADDRESS").expect("ACCOUNT1_ADDRESS is not set");
+        let account1_pk = env::var("ACCOUNT1_PK").expect("ACCOUNT1_PK is not set");
+        let account = Account {
+            address: account1_address.parse::<web3::types::Address>().unwrap(),
+            pk: SecretKey::from_str(account1_pk.as_str()).unwrap()
+        };
+        self.accounts.insert("A1", account);
+
+        let account2_address = env::var("ACCOUNT2_ADDRESS").expect("ACCOUNT2_ADDRESS is not set");
+        let account2_pk = env::var("ACCOUNT2_PK").expect("ACCOUNT2_PK is not set");
+        let account = Account {
+            address: account2_address.parse::<web3::types::Address>().unwrap(),
+            pk: SecretKey::from_str(account2_pk.as_str()).unwrap()
+        };
+        self.accounts.insert("A2", account);
+
+        let mut accounts = self.network.eth().accounts().await?;
+        accounts.push(self.accounts["A1"].address);
+        accounts.push(self.accounts["A2"].address);
+
+        println!("Accounts: {:?}", accounts);
+        
+        Ok(())
+    }
+
+    pub async fn print_balances(&self) -> web3::error::Result {
+        println!("Calling balance.");
+        for account in &self.accounts {
+            let balance = self.network.eth().balance(account.1.address, None).await?;
+            println!("Balance of {:?}: {}", account.1.address, balance);
+        }    
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+use std::sync::Arc;
+use std::time::Duration;
 use std::env;
+use std::str::FromStr;
 use dotenv::dotenv;
 use clap::{App, Arg};
+
+pub mod golem;
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
     println!("Hello Golem!");
 
+    dotenv().ok();
+
     let path = std::env::current_dir().unwrap();
     println!("working dir: '{}'", path.display());
 
-    let mut _network_url: String = "127.0.0.1:8545".to_string();
+    let mut network_url: String = "127.0.0.1:8545".to_string();
+    let mut balance_refresh_period = Duration::from_secs(5);
     
     let matches = App::new("golem-cli")
         .version("0.1")
@@ -24,31 +32,46 @@ async fn main() -> web3::Result<()> {
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("buyers")
+            Arg::with_name("balance")
                 .short("b")
-                .long("buyers")
+                .long("balance")
                 .value_name("1")
-                .help("Sets a number of concurrent buyers asking for GOL")
+                .help("if present program will monitor balance on accounts every x seconds")
                 .takes_value(true),
         ).get_matches();
 
-        match matches.value_of("network").unwrap() {
-            "local" => _network_url = "127.0.0.1:8545".to_string(),
-            "ganache" => _network_url = "127.0.0.1:8545".to_string(),
-            "rinkeby" => {
-                dotenv().ok();
-                _network_url = {
-                    let project_id = env::var("INFURA_PROJECT_ID").expect("INFURA_PROJECT_ID is not set");
-                    format!("https://rinkeby.infura.io/v3/{}", project_id)
-                };
-            },
-            //_ => unreachable!("see possible parameters with golem-cli --help"),
-            _ => (),
-        };
+    match matches.value_of("network").unwrap() {
+        "local" => network_url = "127.0.0.1:8545".to_string(),
+        "ganache" => network_url = "127.0.0.1:8545".to_string(),
+        "rinkeby" => {
+            network_url = {
+                let project_id = env::var("INFURA_PROJECT_ID").expect("INFURA_PROJECT_ID is not set");
+                format!("https://rinkeby.infura.io/v3/{}", project_id)
+            };
+        },
+        _ => unreachable!("see possible parameters with golem-cli --help"),
+    };
 
-    println!("using URL: {}", _network_url);
+    println!("using URL: {}", network_url);
 
-    
+    match matches.value_of("balance").unwrap() {
+        x => balance_refresh_period = Duration::from_secs(u64::from_str(x).unwrap())
+    };
+
+    let mut golem = golem::GolemToken::new(&network_url);
+    golem.initialize_accounts().await?;
+
+    let golem_for_tasks = Arc::new(golem).clone();
+
+    //display accounts balances
+    let h = tokio::spawn(async move {
+        golem_for_tasks.print_balances().await.unwrap();
+    });
+
+    let _t1 = match h.await {
+        Ok(_) => println!("balance completed"),
+        Err(e) => println!("balance failed: {:?}", e)
+    };
 
     Ok(())
 }
